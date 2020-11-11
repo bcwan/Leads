520b7827ed362dce7e743563c503239304768185
bcwan <bcwan8@gmail.com>
2020-11-11 00:11:26 -0800

    Shift Django to use PostgreSQL

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'leadmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'leads',
+        'USER': 'djangoleads',
+        'PASSWORD': 'djangoleads123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
