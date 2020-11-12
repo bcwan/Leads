a128f1a29e741a3d7e71f4fa213f50309b2080c5
bcwan <bcwan8@gmail.com>
2020-11-11 18:17:00 -0800

    Add comments in urls for future reference

--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# include directs all data sent in / route to 'leads.url'
 urlpatterns = [
     path('', include('leads.urls')),
 ]
