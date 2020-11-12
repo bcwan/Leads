81a550219d48fd02f76035efc8d10a9432f58cb8
bcwan <bcwan8@gmail.com>
2020-11-11 21:52:04 -0800

    Add Header component to App.js

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from '../components/layout/Header';
+
 class App extends Component {
   render() {
-    return <h1>React App</h1>
+    return (
+      <Header />
+    )
   }
 }
 
